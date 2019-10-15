36c2e1ad32ca5bb190f187f81259c01b5cc11a44
Thomas van Dam <t.vandam@wearetriple.com>
2019-10-15 11:42:12 +0200

    feat: Move to TypeScript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `thomasvandam.com`,
+    description: `Public diary.`,
+    author: `@saDammie`,
   },
   plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-tslint`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,14 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-
+var path = require('path');
 // You can delete this file if you're not using it
+exports.onCreateWebpackConfig = function({ actions }) {
+    actions.setWebpackConfig({
+        resolve: {
+            alias: {
+                '~': path.resolve(__dirname, 'src')
+            }
+        }
+    })
+}
