883bbdca89a0f061a4bd18b47fc7bc1210f8f20d
Thomas van Dam <t.vandam@wearetriple.com>
2019-10-22 18:10:05 +0200

    feat: Update blog posts to show labels

---
+++ src/templates/regularPost/regularPost.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import $ from './regularPost.module.scss';
+
+import Layout from '~/components/layout/layout';
+import SEO from '~/components/seo';
+import DateLabel from '~/components/date/dateLabel';
+import TagLabel from '~/components/tagLabel/tagLabel';
+import FirstWordHighlight from '~/components/firstWordHighlight/firstWordHighlight';
+
+const RegularPostTemplate = ({ data, pageContext }) => {
+    const { markdownRemark } = data;
+    const { frontmatter: { title, date, tags }, html } = markdownRemark;
+
+    return (
+        <Layout>
+            <SEO title={title} />
+            <div className={$.titleContainer}>
+                <h2 className={$.title}><FirstWordHighlight>{title}</FirstWordHighlight></h2>
+                <DateLabel date={date}/>
+            </div>
+
+            <div className={$.tagContainer}>
+                {tags.map(tag => (<TagLabel key={tag} tag={tag}/>))}
+            </div>
+
+            <div dangerouslySetInnerHTML={{ __html: html }}/>
+        </Layout>
+    );
+};
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        tags
+      }
+    }
+  }
+`;
+
+export default RegularPostTemplate;
