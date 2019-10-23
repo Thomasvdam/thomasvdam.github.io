7c493a7e6f97a4874012040775c2000d8735dbce
Travis CI User <travis@example.org>
2019-10-23 19:37:16 +0000

    Updates

---
+++ component---src-pages-404-tsx-29b327654d5f375773cd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{226:function(e,t,n){"use strict";n.r(t);var a=n(0),l=n.n(a),o=n(220),u=n(219);t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement("h2",null,"Not found"),l.a.createElement("p",null,"We did find this 404 page though so at least that's something."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-29b327654d5f375773cd.js.map
