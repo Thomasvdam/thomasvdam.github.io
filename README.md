549e561cd1bde030f4032d4e23d5913816ee0b64
Thomas van Dam <t.vandam@wearetriple.com>
2019-10-24 15:09:21 +0200

    feat: Add sitemap generation
    I'm not sure whether the priorities are set correctly, but given that I
    don't particularly want to score well in search engines (yet) I'm happy
    enough.

--- README.md
+++ README.md
@@ -8,4 +8,3 @@ Made using Gatsby and hosted on GitHub pages.
 - [ ] Rework navigational structure + code
 - [ ] Rename tags to categories throughout the codebase. Other way around is also fine, not sure what I prefer yet.
 - [ ] Landing page
-- [ ] SSL
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,58 +1,86 @@
+const DOMAIN = 'https://thomasvandam.com';
+
 module.exports = {
-  siteMetadata: {
-    title: `cat /etc/thoughts`,
-    description: `Public diary.`,
-    author: `@saDammie`,
-  },
-  plugins: [
-    'gatsby-plugin-sass',
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-tslint`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        author: `@saDammie`,
+        description: `Public diary.`,
+        siteUrl: DOMAIN,
+        title: `cat /etc/thoughts`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/src/pages/markdown`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
+    plugins: [
+        'gatsby-plugin-sass',
+        `gatsby-plugin-typescript`,
+        `gatsby-plugin-tslint`,
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              maxWidth: 600,
-              backgroundColor: "transparent",
+                name: `images`,
+                path: `${__dirname}/src/images`,
             },
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `thomasvandam.com`,
-        short_name: `thomasvandam.com`,
-        start_url: `/`,
-        background_color: `#F2F2F2`,
-        theme_color: `#434343`,
-        display: `minimal-ui`,
-        icon: `src/images/batman-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `markdown-pages`,
+                path: `${__dirname}/src/pages/markdown`,
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 600,
+                            backgroundColor: "transparent",
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `thomasvandam.com`,
+                short_name: `thomasvandam.com`,
+                start_url: `/`,
+                background_color: `#F2F2F2`,
+                theme_color: `#434343`,
+                display: `minimal-ui`,
+                icon: `src/images/batman-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-sitemap',
+            options: {
+                serialize: ({ site, allSitePage }) => {
+                    return allSitePage.edges.map(edge => {
+                        const { path } = edge.node;
+                        const sitemapConfig = {
+                            changefreq: `weekly`,
+                            priority: 0.7,
+                            url: site.siteMetadata.siteUrl + path,
+                        }
+
+                        if (path === '/' || path.startsWith('/category')) {
+                            sitemapConfig.changefreq = 'daily';
+                            sitemapConfig.priority = 1;
+                        } else if (path.startsWith('/post')) {
+                            sitemapConfig.changefreq = 'monthly';
+                            sitemapConfig.priority = 0.5;
+                        }
+
+                        return sitemapConfig;
+                    });
+                },
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
 }
